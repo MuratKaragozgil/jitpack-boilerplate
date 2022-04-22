815ddfad68439533f45688d9e75bf825528e6565
Murat Karagözgil <m.karagozgil@gmail.com>
2022-04-22 16:16:28 +0300

    Initial commit

---
+++ src/main/java/com/jitpack/jitpackboilerplate/JitpackBoilerplateApplication.java
@@ -0,0 +1,8 @@
+package com.jitpack.jitpackboilerplate;
+
+public class JitpackBoilerplateApplication {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/main/java/com/jitpack/jitpackboilerplate/JitpackSampleClass.java
@@ -0,0 +1,15 @@
+package com.jitpack.jitpackboilerplate;
+
+import lombok.Data;
+
+/**
+ * @author Murat Karagozgil
+ * @since 22.04.2022, Friday
+ */
+@Data
+public class JitpackSampleClass {
+
+    private Integer id;
+    private String name;
+
+}
